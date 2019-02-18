d5b01f196aacc8184e60829d1ae4e8a09a0820db
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-02-18 20:02:48 +0100

    link test in compile script, folders, bin

--- compile.py
+++ compile.py
@@ -5,17 +5,23 @@ import os
 def pjoin(path_list):
     return ' '.join(path_list)
 
+def mkdir_try(path):
+    try:
+        os.mkdir(path)
+    except:
+        pass
+
 src_dir = 'adlib'
 test_dir = 'test'
 include_dir = 'include'
 build_dir = 'build'
 obj_dir = os.path.join(build_dir, 'obj')
+bin_dir = 'bin'
 
-try:
-    os.mkdir(build_dir)
-    os.mkdir(obj_dir)
-except:
-    pass
+mkdir_try(build_dir)
+mkdir_try(obj_dir)
+mkdir_try(bin_dir)
+
 
 includes = '-I ' + os.getcwd() + ' -I ' + include_dir + ' -I ' + src_dir
 
@@ -31,28 +37,26 @@ test_paths = [os.path.join(test_dir, f) + '.cc' for f in test_files];
 
 obj_paths = [os.path.join(obj_dir, f) + '.o' for f in src_files];
 test_obj_paths = [os.path.join(obj_dir, f) + '.o' for f in test_files];
+test_bin_paths = [os.path.join(bin_dir, f) for f in test_files];
 
 # compile command string
-hasError = False
 for s,o in zip(src_paths,obj_paths):
     print("Compiling source "+s+"....")
-    if hasError:
-        break
     compile_str = 'g++ -c '  + s +  ' ' + includes + ' -o ' + o
     p = subprocess.run(compile_str, shell=True)
     hasError = p.returncode
+    if hasError:
+        break
 
 for s,o in zip(test_paths,test_obj_paths):
     print("Compiling test "+s+"....")
-    if hasError:
-        break
     compile_str = 'g++ -c '  + s +  ' ' + includes + ' -o ' + o
     p = subprocess.run(compile_str, shell=True)
     hasError = p.returncode
+    if hasError:
+        break
 
-link_str = "g++ -o test_expression " + " ".join(obj_paths) + ' ' + " ".join(test_obj_paths)
-
-p = subprocess.run(compile_str, shell=True)
-if p.returncode == 0:
-    print("Linking....")
+for o,b in zip(test_obj_paths,test_bin_paths):
+    link_str = "g++ " + o + ' ' + pjoin(obj_paths) + ' -o ' + b
+    print("Linking test " + b  + " ....")
     subprocess.run(link_str, shell=True)
